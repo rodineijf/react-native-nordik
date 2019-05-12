6e26fd8a1aa1bb76d3cfdecef5f2dda2b0575943
Rodinei Fagundes <rodinei.jf@gmail.com>
2019-05-11 21:04:02 -0300

    add backgroundColor props

--- src/components/ScrollView/index.js
+++ src/components/ScrollView/index.js
@@ -1,18 +1,31 @@
 import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleProp, StyleSheet, View, ViewStyle } from "react-native";
 import { compose } from "recompose";
 
 import { hp, wp } from "../../utils";
 import { withLoader } from "../withLoader";
 
-function NkScrollView({ children, containerStyle }) {
+type Props = {
+  children: React.ReactNode,
+  containerStyle: StyleProp<ViewStyle>,
+  backgroundColor: string,
+};
+
+function NkScrollView({ children, containerStyle, backgroundColor = "transparent" }: Props) {
+  const outterContainerStyle = {
+    backgroundColor,
+    flex: 1,
+  };
+
   return (
-    <ScrollView
-      contentContainerStyle={[styles.contentContainerStyle, containerStyle]}
-      keyboardShouldPersistTaps="handled"
-    >
-      {children}
-    </ScrollView>
+    <View style={outterContainerStyle}>
+      <ScrollView
+        contentContainerStyle={[styles.contentContainerStyle, containerStyle]}
+        keyboardShouldPersistTaps="handled"
+      >
+        {children}
+      </ScrollView>
+    </View>
   );
 }
 
